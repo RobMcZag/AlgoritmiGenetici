539516077646f3c042ccab7690f10d14d43483c9
Roberto Zagni <rz70rz70@gmail.com>
2015-01-25 00:26:26 +0100

    Changed main to play with the new Consistent code breaker

--- MasterMind/src/main/java/com/rzagni/mastermind/MasterMind.java
+++ MasterMind/src/main/java/com/rzagni/mastermind/MasterMind.java
@@ -8,7 +8,8 @@ public class MasterMind {
 		int codeLength = 4;
 		int numberOfColors = 6;
 		CodeMaker<Integer> cm = new IntegerCodeMaker( codeLength, numberOfColors);
-		CodeBreaker<Integer> cb = new IntegerCodeBreaker(codeLength, numberOfColors);
+//		CodeBreaker<Integer> cb = new IntegerCodeBreaker(codeLength, numberOfColors);
+		CodeBreaker<Integer> cb = new IntegerConsistentCodeBreaker(codeLength, numberOfColors);
 		
 		int[] res = new int[ResultMarker.values().length];
 		ResultMarker[] result = new ResultMarker[codeLength];
@@ -18,16 +19,17 @@ public class MasterMind {
 			Peg<Integer>[] guess = cb.getNextGuess(result);
 			result = cm.evaluateGuess(guess);
 			res = ResultMarker.countResultMarkers(result);
-			logInfo(++guessCounter);
+			logInfo(guess, result, ++guessCounter);
 		}
 		System.out.println("\nSoluzione trovata in " + guessCounter + " tentativi");
+		System.out.println(Arrays.toString(((IntegerCodeMaker) cm).getSecretCode()));
 	}
 
-	private static void logInfo(int guessCounter) {
-		System.out.print(".");
-		if ((guessCounter % 50) == 0) {
-			System.out.println(" - " + guessCounter);
-		}
+	private static void logInfo(Peg<Integer>[] guess, ResultMarker[] result, int guessCounter) {
+		System.out.println(Arrays.toString(guess) + " => " + Arrays.toString(result) + " - " + guessCounter);
+//		if ((guessCounter % 50) == 0) {
+//			System.out.println(" - " + guessCounter);
+//		}
 	}
 
 }
