549f508b565b7defa89b4dccdf3fc682a4b587aa
Roberto Zagni <rz70rz70@gmail.com>
2015-01-24 21:00:11 +0100

    Added static method countResultMarkers(ResultMarkers[]) as an utility method to have an easy breakdown of results by Marker type.

--- MasterMind/src/main/java/com/rzagni/mastermind/ResultMarker.java
+++ MasterMind/src/main/java/com/rzagni/mastermind/ResultMarker.java
@@ -1,9 +1,9 @@
 package com.rzagni.mastermind;
 
 public enum ResultMarker {
-	EMPTY("One wrong color independent of position"),	// Wrong color independent of position
+	BLACK("One right color in right position"),	// Right color in right position
 	WHITE("One right color in wrong position"),	// Right color in wrong position
-	BLACK("One right color in right position");	// Right color in right position
+	EMPTY("One wrong color independent of position");	// Wrong color independent of position
 	
 	private final String description;
 	
@@ -18,4 +18,16 @@ public enum ResultMarker {
 		return description;
 	}
 	
+	/**
+	 * Counts the ResultMarkers of each type.
+	 * @param result the result to count markers in;
+	 * @return an array with the count for every Marker in their ordinal order;
+	 */
+	public static int[] countResultMarkers(ResultMarker[] result) {
+		int[] count = new int[ResultMarker.values().length];
+		for (ResultMarker resultMarker : result) {
+			count[resultMarker.ordinal()]++;
+		}
+		return count;
+	}
 }
--- MasterMind/src/test/java/com/rzagni/mastermind/ResultMarkerTest.java
+++ MasterMind/src/test/java/com/rzagni/mastermind/ResultMarkerTest.java
@@ -20,8 +20,53 @@ public class ResultMarkerTest {
 	/* Just for my understanding of enums*/
 	@Ignore @Test
 	public void testValues() {
-		assertEquals(0, ResultMarker.EMPTY.ordinal());
+		assertEquals(2, ResultMarker.EMPTY.ordinal());
 		assertEquals("EMPTY", ResultMarker.EMPTY.name());
 		assertEquals(ResultMarker.EMPTY, ResultMarker.valueOf("EMPTY"));
 	}
+	
+	@Test(expected=NullPointerException.class)
+	public void testCountResultMarkers_NullArray() {
+		ResultMarker.countResultMarkers(null);
+		fail("Should throw a NullPointerException if the array is a null.");
+	}
+	@Test(expected=NullPointerException.class)
+	public void testCountResultMarkers_NullValues() {
+		ResultMarker.countResultMarkers(
+				new ResultMarker[] {ResultMarker.EMPTY, null, ResultMarker.EMPTY, ResultMarker.EMPTY});
+		fail("Should throw a NullPointerException if there is a null in the values of the array.");
+	}
+	@Test
+	public void testCountResultMarkers() {
+		int [] res = ResultMarker.countResultMarkers(
+				new ResultMarker[] {ResultMarker.EMPTY, ResultMarker.EMPTY, ResultMarker.EMPTY});
+		assertEquals(0, res[ResultMarker.BLACK.ordinal()]);
+		assertEquals(0, res[ResultMarker.WHITE.ordinal()]);
+		assertEquals(3, res[ResultMarker.EMPTY.ordinal()]);
+
+		res = ResultMarker.countResultMarkers(
+				new ResultMarker[] {ResultMarker.BLACK, ResultMarker.WHITE, ResultMarker.EMPTY});
+		assertEquals(1, res[ResultMarker.BLACK.ordinal()]);
+		assertEquals(1, res[ResultMarker.WHITE.ordinal()]);
+		assertEquals(1, res[ResultMarker.EMPTY.ordinal()]);
+
+		res = ResultMarker.countResultMarkers(
+				new ResultMarker[] {ResultMarker.BLACK, ResultMarker.WHITE, ResultMarker.EMPTY, ResultMarker.EMPTY});
+		assertEquals(1, res[ResultMarker.BLACK.ordinal()]);
+		assertEquals(1, res[ResultMarker.WHITE.ordinal()]);
+		assertEquals(2, res[ResultMarker.EMPTY.ordinal()]);
+
+		res = ResultMarker.countResultMarkers(
+				new ResultMarker[] {ResultMarker.BLACK, ResultMarker.WHITE, ResultMarker.EMPTY, ResultMarker.BLACK});
+		assertEquals(2, res[ResultMarker.BLACK.ordinal()]);
+		assertEquals(1, res[ResultMarker.WHITE.ordinal()]);
+		assertEquals(1, res[ResultMarker.EMPTY.ordinal()]);
+
+		res = ResultMarker.countResultMarkers(
+				new ResultMarker[] {ResultMarker.BLACK, ResultMarker.WHITE, ResultMarker.BLACK, ResultMarker.EMPTY, ResultMarker.BLACK});
+		assertEquals(3, res[ResultMarker.BLACK.ordinal()]);
+		assertEquals(1, res[ResultMarker.WHITE.ordinal()]);
+		assertEquals(1, res[ResultMarker.EMPTY.ordinal()]);
+	}
+	
 }
