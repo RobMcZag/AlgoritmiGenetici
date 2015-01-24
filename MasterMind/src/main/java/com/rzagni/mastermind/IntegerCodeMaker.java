07373d7a9118161d6c91c5b7ea0a32943a19ed9d
Roberto Zagni <rz70rz70@gmail.com>
2015-01-24 21:20:05 +0100

    Added method evaluateGuess to evaluate a Guess in respect to the current secret pattern, delegating to super class evaluatePattern.

--- MasterMind/src/main/java/com/rzagni/mastermind/IntegerCodeMaker.java
+++ MasterMind/src/main/java/com/rzagni/mastermind/IntegerCodeMaker.java
@@ -6,7 +6,7 @@ public class IntegerCodeMaker extends IntegerCoder {
 	private final Peg<Integer>[] secretPattern;
 
 	/**
-	 * Creates a code maker that will create a pattern of the desired length,
+	 * Creates a code maker that will create a secret pattern of the desired length,
 	 * using the desired number of colors
 	 * @param codeLength The desired length for the pattern. Minimum is 1.
 	 * @param numberOfColors The desired number of colors to chose from to create the pattern. Minimum is 2.
@@ -19,6 +19,19 @@ public class IntegerCodeMaker extends IntegerCoder {
 
 	public Peg<Integer>[] getSecretCode() {
 		return this.secretPattern;
+	}
+
+	/**
+	 * Evaluates the guess in respect to the secret pattern.
+	 * Gives a BLACK ResultMarker for every Peg of the right color in the right position,
+	 * and a WHITE peg for every Peg of the right color in the wrong position,
+	 * Pattern and guess should be the same length, as well as the Result.
+	 * @param pattern the pattern to be compared with the guess
+	 * @param guess the guess to be compared with the pattern 
+	 * @return the result of the evaluation
+	 */
+	public ResultMarker[] evaluateGuess(Peg<Integer>[] guess) {
+		return evaluatePattern(this.secretPattern, guess);
 	}
 
 }
